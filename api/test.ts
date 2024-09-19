3f25194546835307685b9c61b27069cc627d1195
AlexCPT-cpu <alexodey79@gmail.com>
2024-09-19 23:17:15 +0100

    test

--- api/test.ts
+++ api/test.ts
@@ -1,6 +1,11 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-//@ts-expect-error expect
 export default function (req: VercelRequest, res: VercelResponse) {
-  res.status(200).send("API is working!");
+  if (req.method === "GET") {
+    try {
+      res.status(200).send("API is working!");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist", // Vercel expects your static files here
   },
 });
