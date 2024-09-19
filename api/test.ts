b5df56e33bc01b6d24de58f164994306b6c54dfd
AlexCPT-cpu <alexodey79@gmail.com>
2024-09-20 00:59:24 +0100

    f

--- api/test.ts
+++ api/test.ts
@@ -1,16 +1,11 @@
-// import { VercelRequest, VercelResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-// export default function handler(req: VercelRequest, res: VercelResponse) {
-//   if (req.method === "GET") {
-//     try {
-//       res.status(200).send("API is working!");
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// }
-
-export function GET(request: Request) {
-  console.log(request);
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "GET") {
+    try {
+      res.status(200).send("API is working!");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
