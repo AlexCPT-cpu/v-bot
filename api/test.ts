876770d03be5d8060f28b7cb0a5d593a8bd0685a
AlexCPT-cpu <alexodey79@gmail.com>
2024-09-19 22:56:25 +0100

    update

--- api/test.ts
+++ api/test.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-//@ts-expect-error request object not defined
 
+//@ts-expect-error expect
 export default function (req: VercelRequest, res: VercelResponse) {
   res.status(200).send("API is working!");
 }
