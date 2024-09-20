bf4f9e97dc6d40f75faba5ac9cce50b7fd193435
AlexCPT-cpu <alexodey79@gmail.com>
2024-09-20 01:27:56 +0100

    common js modules

---
+++ api/test.cjs
@@ -0,0 +1,21 @@
+// export default function handler(req, res) {
+//   if (req.method === "GET") {
+//     try {
+//       res.status(200).send("API is working!");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+
+// const { VercelRequest, VercelResponse } = require("@vercel/node");
+
+module.exports = function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      res.status(200).send("API is working!");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
--- api/test.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      res.status(200).send("API is working!");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
